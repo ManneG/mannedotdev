f0ec5ab7e22c3cfebe57315b9e8202580fdf467c
Manne <74592735+ManneG@users.noreply.github.com>
2024-11-21 16:34:33 +0100

    Print for debugging

--- main.go
+++ main.go
@@ -8,11 +8,18 @@ import (
 
 func main() {
 	http.HandleFunc("GET /{$}", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println(r.URL.Path)
 		http.ServeFile(w, r, "./index.html")
 	})
 
 	http.HandleFunc("GET /info", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println(r.URL.Path)
 		fmt.Fprintf(w, "You are %s visiting %s using %s.", r.RemoteAddr, r.Host, r.Proto)
+	})
+
+	http.HandleFunc("/*", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("404: " + r.URL.Path)
+		http.NotFound(w, r)
 	})
 
 	log.Print("Listening on :8080")
